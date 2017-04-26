2fab9b1ab7a1cdc4756726f80b0194f712323315
Silvanei Soares Santos <silvanei@dccontrol.com.br>
2017-04-26 10:59:47 -0300

    FIX FlashNotifi

--- module/Common/src/View/Helper/FlashNotify.php
+++ module/Common/src/View/Helper/FlashNotify.php
@@ -14,10 +14,10 @@ use Zend\View\Helper\InlineScript;
 
 class FlashNotify extends AbstractHelper
 {
-    /** @var FlashMessenger  */
+    /** @var FlashMessenger */
     private $flashMessenger;
 
-    /** @var InlineScript  */
+    /** @var InlineScript */
     private $inlineScript;
 
     /**
@@ -28,18 +28,18 @@ class FlashNotify extends AbstractHelper
     public function __construct(FlashMessenger $flashMessenger, InlineScript $inlineScript)
     {
         $this->flashMessenger = $flashMessenger;
-        $this->inlineScript   = $inlineScript;
+        $this->inlineScript = $inlineScript;
     }
 
     public function __invoke(): void
     {
-        $plugin   = $this->flashMessenger;
-        $notify     = [
-            'alert'       => array_merge($plugin->getMessages(), $plugin->getCurrentMessages()),
-            'information' => array_merge($plugin->getInfoMessages(), $plugin->getCurrentInfoMessages()),
-            'success'     => array_merge($plugin->getSuccessMessages(), $plugin->getCurrentSuccessMessages()),
-            'warning'     => array_merge($plugin->getWarningMessages(), $plugin->getCurrentWarningMessages()),
-            'error'       => array_merge($plugin->getErrorMessages(), $plugin->getCurrentErrorMessages()),
+        $plugin = $this->flashMessenger;
+        $notify = [
+            'default' => array_merge($plugin->getMessages(), $plugin->getCurrentMessages()),
+            'info'    => array_merge($plugin->getInfoMessages(), $plugin->getCurrentInfoMessages()),
+            'success' => array_merge($plugin->getSuccessMessages(), $plugin->getCurrentSuccessMessages()),
+            'warn'    => array_merge($plugin->getWarningMessages(), $plugin->getCurrentWarningMessages()),
+            'error'   => array_merge($plugin->getErrorMessages(), $plugin->getCurrentErrorMessages()),
         ];
 
         $plugin->clearCurrentMessages('default');
@@ -49,12 +49,16 @@ class FlashNotify extends AbstractHelper
         $plugin->clearCurrentMessages('error');
 
         $this->inlineScript->captureStart();
-        foreach(array_filter($notify) as $type => $messages){
-            $message = implode('<br/><br/>', $messages);
-            $message = preg_replace('/\s+/', ' ', $message);
-            $message = str_replace("'", '&#34;', $message);
+        foreach (array_filter($notify) as $type => $messages) {
+            echo '$.notify.defaults({ className: "success" });';
 
-            echo sprintf('$.notify("%s", "%s");', $message, $type);
+            foreach ($messages as $message) {
+                if ($type === 'default') {
+                    echo sprintf('$.notify("%s");', $message);
+                } else {
+                    echo sprintf('$.notify("%s", "%s");', $message, $type);
+                }
+            }
         }
         $this->inlineScript->captureEnd();
     }
